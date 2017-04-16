d3983ee1b05586cd405292b0d25929c43fc0544c
unknown <ericjiang97@gmail.com>
2017-04-16 10:12:14 +1000

    Updated LinkedList Class

--- Chapter 2 - Linked Lists/linkedlist.py
+++ Chapter 2 - Linked Lists/linkedlist.py
@@ -21,6 +21,19 @@ class linkedList:
         newNode.next = self.currentNode   # link the new node to the 'previous' node.
         self.currentNode = newNode        #  set the current node to the new one.
 
+    def removeNode(self, location):
+        '''
+        Remove a Node from the LinkedList
+        :param head: The previous position
+        :param next: The next value of the linkedlist
+        '''
+        node = self.currentNode
+        count = 0
+        while count != location:
+            node = node.next
+            count += 1
+        node.next = node.next.next
+
     def printList(self):
         '''
         Prints the LinkedList
@@ -34,5 +47,6 @@ ll = linkedList()
 ll.addNode(1)
 ll.addNode(2)
 ll.addNode(3)
+ll.removeNode(2)
 
 ll.printList()
